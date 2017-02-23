abc1757981d24352e5092e5a3a69fe14083fbbde
M Reza Nurrifqi <reza.rifqi@yahoo.com>
2017-02-23 16:41:23 +0700

    Add admin functionality

--- src/app/admin/productTable.component.ts
+++ src/app/admin/productTable.component.ts
@@ -1,7 +1,18 @@
 import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
+
 @Component({
-    template: `<div style="padding: 10px;">
-                <h3>Product Table Placeholder</h3>
-              </div>`
+    moduleId: module.id,
+    templateUrl: "productTable.component.html"
 })
-export class ProductTableComponent {}
+export class ProductTableComponent {
+  constructor(private repository: ProductRepository) { }
+    getProducts(): Product[] {
+        return this.repository.getProducts();
+    }
+
+    deleteProduct(id: number) {
+        this.repository.deleteProduct(id);
+    }
+}
