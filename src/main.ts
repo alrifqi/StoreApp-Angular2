1ca2952c00b4a2edf55b5b32f40bcfb264c0d6f4
M Reza Nurrifqi <reza.rifqi@yahoo.com>
2017-02-24 10:37:14 +0700

    Add configuration for build

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+///<reference path="../typings/globals/core-js/index.d.ts"/>
+///<reference path="../typings/globals/node/index.d.ts"/>
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from "@angular/platform-browser";
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
+// import { AppModuleNgFactory } from "../aot/app/app.module.ngfactory";
 import { AppModule } from './app/app.module';
 
 if (environment.production) {
@@ -8,3 +12,4 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
